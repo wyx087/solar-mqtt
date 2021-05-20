a666461ea973c4cc662dbd11d31f3b438109cfa1
Wuyan <wuyanxu@gmail.com>
2021-05-20 20:37:31 +0100

    Added choice for normal mode

--- pc_run.bat
+++ pc_run.bat
@@ -1,8 +1,11 @@
 @echo off 
+:: To enable display of timed out choice 
+setlocal enableDelayedExpansion  
+set normalinitialwait=20
 
+::If in above range, run night mode. When hour changes to %nighthourhigh%, GOTO NIGHTFINISH   
 set nighthourlow=00
 set nighthourhigh=7
-::If in above range, run night mode. When hour changes to %nighthourhigh%, GOTO NIGHTFINISH   
 
 :: Local time 
 rem set hour=%time:~0,2%
@@ -20,9 +23,21 @@ IF %hour% GEQ %nighthourlow% IF %hour% LSS %nighthourhigh% (GOTO NIGHTRUN)
 :NORMAL 
 echo Day time, run normally: 
 
-    TIMEOUT /T 15 
-    start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile4
-    TIMEOUT /T 1 > nul
+  for /l %%N in (%normalinitialwait% -1 1) do (
+    set /a "min=%%N/60, sec=%%N%%60, n-=1"
+    if !sec! lss 10 set sec=0!sec!
+    cls
+    choice /c:CN1 /n /m "Start mining in !min!:!sec! - N to Start Now, or C to Cancel: " /t:1 /d:1
+    if not errorlevel 3 goto CHOICEBREAK
+  )
+  cls
+  echo Start mining in 0:00 - N to Start Now, or C to Cancel..
+  :CHOICEBREAK
+  if errorlevel 2 (goto NORMALSTART) else (goto END)  
+    
+  :NORMALSTART
+  start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile4
+    TIMEOUT /T 2 > nul
     :TERMINATE_NORMAL 
         taskkill /IM MSIAfterburner.exe
         TIMEOUT /T 1 > nul
@@ -34,13 +49,13 @@ echo Day time, run normally:
 	:: Above line says if (process not running) else (running).  
 
   :RUN_C_MINING 
-    tasklist /fi "imagename eq Chia.exe" |find "Chia.exe" > nul
+    tasklist /fi "imagename eq chia.exe" |find "chia.exe" > nul
     if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 1 25 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 1 25 )
     :: Above line says if (process not running) else (running).  
   GOTO END
 
   :RUN_C_NOT_M 
-    tasklist /fi "imagename eq Chia.exe" |find "Chia.exe" > nul
+    tasklist /fi "imagename eq chia.exe" |find "chia.exe" > nul
     if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 0 25 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 0 25 )
     :: Above line says if (process not running) else (running).  
   GOTO END 
@@ -49,7 +64,7 @@ echo Day time, run normally:
 :NIGHTRUN
 echo Night time direct mining: 
 
-	TIMEOUT /T 60
+	TIMEOUT /T 30
 	rem turn off display using this: c:\windows\system32\DisplaySwitch /external
 	
 	start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile4
@@ -60,7 +75,7 @@ echo Night time direct mining:
         tasklist /fi "imagename eq MSIAfterburner.exe" |find "MSIAfterburner.exe" > nul
         if errorlevel 1   (GOTO CONTINUE_NIGHT )    else    (GOTO TERMINATE_NIGHT )
     :CONTINUE_NIGHT 
-  "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --power-limit=152
+  "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --power-limit=150
 	start "" "c:\Users\wyx\AppData\Local\Programs\NiceHash Miner\NiceHashMiner.exe"
 
 	:LOOP
@@ -71,7 +86,7 @@ echo Night time direct mining:
 
 	:NIGHTFINISH
 	echo Ending night time mining. Transition to Daytime program: 
-	tasklist /fi "imagename eq Chia.exe" |find "Chia.exe" > nul
+	tasklist /fi "imagename eq chia.exe" |find "chia.exe" > nul
 	if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 1 2 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 1 2 )
 	:: Above line says if (process not running) else (running).  
 
