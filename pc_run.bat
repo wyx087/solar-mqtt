f74a6ce540f8e4798d66489dd22b9fa1bf6ffbb2
Wuyan <wuyanxu@gmail.com>
2022-05-13 09:21:28 +0100

    Updated batch script for night running.

--- pc_run.bat
+++ pc_run.bat
@@ -4,19 +4,19 @@ setlocal enableDelayedExpansion
 set normalinitialwait=40
 
 ::If in above range, run night mode. When hour changes to %nighthourhigh%, GOTO NIGHTFINISH   
-set nighthourlow=00
-set nighthourhigh=6
+set nighthourlow=02
+set nighthourhigh=06
 
 :: Local time 
-rem set hour=%time:~0,2%
-rem if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
-rem echo hour=%hour%
-rem set min=%time:~3,2%
-rem if "%min:~0,1%" == " " set min=0%min:~1,1%
-rem echo min=%min%
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+echo hour=%hour%
+set minute=%time:~3,2%
+if "%minute:~0,1%" == " " set minute=0%minute:~1,1%
+echo minute=%minute%
 :: UTC 
-for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x 
-echo hour=%hour%; minute=%minute%
+rem for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x 
+rem echo hour=%hour%; minute=%minute%
 
 IF %hour% GEQ %nighthourlow% IF %hour% LSS %nighthourhigh% (GOTO NIGHTRUN)
 
@@ -40,10 +40,10 @@ echo Day time, run normally:
   start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile4
     TIMEOUT /T 2 > nul
     :TERMINATE_NORMAL 
-        taskkill /IM MSIAfterburner.exe
-        TIMEOUT /T 2 > nul
-        tasklist /fi "imagename eq MSIAfterburner.exe" |find "MSIAfterburner.exe" > nul
-        if errorlevel 1   (GOTO CONTINUE_NORMAL )    else    (GOTO TERMINATE_NORMAL )
+rem        taskkill /IM MSIAfterburner.exe
+rem        TIMEOUT /T 2 > nul
+rem        tasklist /fi "imagename eq MSIAfterburner.exe" |find "MSIAfterburner.exe" > nul
+rem        if errorlevel 1   (GOTO CONTINUE_NORMAL )    else    (GOTO TERMINATE_NORMAL )
     :CONTINUE_NORMAL 
 	tasklist /fi "imagename eq NiceHashMiner.exe" |find "NiceHashMiner.exe" > nul
 	if errorlevel 1    (GOTO RUN_C_NOT_M)    else    (GOTO RUN_C_MINING)
@@ -51,13 +51,13 @@ echo Day time, run normally:
 
   :RUN_C_MINING 
     tasklist /fi "imagename eq chia.exe" |find "chia.exe" > nul
-    if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 1 25 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 1 25 )
+    if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 1 45 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 1 45 )
     :: Above line says if (process not running) else (running).  
   GOTO END
 
   :RUN_C_NOT_M 
     tasklist /fi "imagename eq chia.exe" |find "chia.exe" > nul
-    if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 0 25 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 0 25 )
+    if errorlevel 1    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 1 1 0 45 )    else    (wsl /mnt/g/programs/NiceHash_auto/pc.exe 0 1 0 45 )
     :: Above line says if (process not running) else (running).  
   GOTO END 
 
@@ -71,18 +71,29 @@ echo Night time direct mining:
 	start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile4
     TIMEOUT /T 5
     :TERMINATE_NIGHT 
-        taskkill /IM MSIAfterburner.exe
-        TIMEOUT /T 2 > nul
-        tasklist /fi "imagename eq MSIAfterburner.exe" |find "MSIAfterburner.exe" > nul
-        if errorlevel 1   (GOTO CONTINUE_NIGHT )    else    (GOTO TERMINATE_NIGHT )
+rem        taskkill /IM MSIAfterburner.exe
+rem        TIMEOUT /T 2 > nul
+rem        tasklist /fi "imagename eq MSIAfterburner.exe" |find "MSIAfterburner.exe" > nul
+rem        if errorlevel 1   (GOTO CONTINUE_NIGHT )    else    (GOTO TERMINATE_NIGHT )
     :CONTINUE_NIGHT 
   "C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --power-limit=128
 	start "" "c:\Users\wyx\AppData\Local\Programs\NiceHash Miner\NiceHashMiner.exe"
 
 	:LOOP
 		TIMEOUT /T 120  > nul
-    "G:\programs\ClickMonitorDDC\ClickMonitorDDC_7_2.exe" x 
-		for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x 
+		"G:\programs\ClickMonitorDDC\ClickMonitorDDC_7_2.exe" x 
+		
+		:: Local time 
+		set hour=%time:~0,2%
+		if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+		echo hour=%hour%
+		set minute=%time:~3,2%
+		if "%minute:~0,1%" == " " set minute=0%minute:~1,1%
+		echo minute=%minute%
+		:: UTC 
+		rem for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x 
+		rem echo hour=%hour%; minute=%minute%
+		
 		IF %hour% GEQ %nighthourhigh%  IF %minute% GEQ 0 (GOTO NIGHTFINISH)
 	GOTO LOOP 
 
